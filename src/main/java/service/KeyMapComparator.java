4dad70f534dc0d6ac2de394942afebc182731096
CarlosVRL <carlos.varela.eng@gmail.com>
2019-01-12 01:33:48 -0600

    Implement get/set operators for Map data structure

--- src/main/java/service/KeyMapComparator.java
+++ src/main/java/service/KeyMapComparator.java
@@ -1,4 +1,29 @@
 package service;
 
-public class KeyMapComparator {
+import java.util.Map;
+
+public class KeyMapComparator
+{
+    private Map<String, String> baselineData;
+    private Map<String, String> externalData;
+
+    public void setBaselineData(Map<String, String> baselineData)
+    {
+        this.baselineData = baselineData;
+    }
+
+    public Map<String, String> getBaselineData()
+    {
+        return this.baselineData;
+    }
+
+    public void setExternalData(Map<String, String> externalData)
+    {
+        this.externalData = externalData;
+    }
+
+    public Map<String, String> getExternalData()
+    {
+        return this.externalData;
+    }
 }
--- src/test/java/service/KeyMapComparatorTest.java
+++ src/test/java/service/KeyMapComparatorTest.java
@@ -1,16 +1,59 @@
 package service;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.testng.Assert.*;
 
-public class KeyMapComparatorTest {
+public class KeyMapComparatorTest
+{
+    private KeyMapComparator keyMapComparator;
+    private Map<String, String> baselineTestData;
+    private Map<String, String> externalTestData;
+
+    @BeforeMethod
+    public void initTest()
+    {
+        keyMapComparator = new KeyMapComparator();
+        initTestData();
+    }
 
     @Test
     public void canInstantiateObject()
     {
-        KeyMapComparator keyMapComparator = new KeyMapComparator();
         assertNotNull(keyMapComparator);
     }
 
+    @Test
+    public void canReadBaselineDataFromMap()
+    {
+        keyMapComparator.setBaselineData(baselineTestData);
+        Map<String, String> baselineData = keyMapComparator.getBaselineData();
+        assertEquals(baselineData, baselineTestData);
+    }
+
+    @Test
+    public void canReadExternalDataFromMap()
+    {
+        keyMapComparator.setExternalData(baselineTestData);
+        Map<String, String> externalData = keyMapComparator.getExternalData();
+        assertEquals(externalData, baselineTestData);
+    }
+
+    private void initTestData()
+    {
+        baselineTestData = new HashMap<String, String>();
+        baselineTestData.put("1", "a");
+        baselineTestData.put("2", "b");
+        baselineTestData.put("3", "c");
+        baselineTestData.put("1", "d");
+
+        externalTestData = new HashMap<String, String>();
+        externalTestData.put("1", "a");
+        externalTestData.put("2", "B");
+        externalTestData.put("4", "e");
+    }
 }
